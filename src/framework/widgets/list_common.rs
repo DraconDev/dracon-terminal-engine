32d0ad45e91beb926c2280ac314755cdb3ed607e
DraconDev <dracsharp@gmail.com>
2026-05-14 18:50:15 +0100

    chore(misc): update list_common
    update list_common
    ---
    category: chore
    scope: misc
    ---

--- src/framework/widgets/list_common.rs
+++ src/framework/widgets/list_common.rs
@@ -19,6 +19,13 @@ pub struct ListNavigation<S: Clone> {
     pub enable_undo: bool,
     pub undo_stack: Vec<S>,
     pub redo_stack: Vec<S>,
+    }
+}
+
+impl<S: Clone> Default for ListNavigation<S> {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl<S: Clone> ListNavigation<S> {
