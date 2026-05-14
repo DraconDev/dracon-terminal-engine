111920f5091856e08f071c15d4666af988171e56
DraconDev <dracsharp@gmail.com>
2026-05-14 15:05:02 +0100

    chore(misc): update confirm_dialog
    update confirm_dialog
    ---
    category: chore
    scope: misc
    ---

--- src/framework/widgets/confirm_dialog.rs
+++ src/framework/widgets/confirm_dialog.rs
@@ -18,6 +18,8 @@
 //! ```
 
 use crate::compositor::{Cell, Color, Plane, Styles};
+use crate::framework::command::BoundCommand;
+use crate::framework::theme::Theme;
 use crate::framework::widget::{Widget, WidgetId};
 use ratatui::layout::Rect;
 
