e30b0674bb242f66102f5659b7bd6ee19cdd8973
DraconDev <dracsharp@gmail.com>
2026-05-16 09:20:15 +0100

    test(utils): add missing move_recursive import in utils_test

--- tests/utils_test.rs
+++ tests/utils_test.rs
@@ -6,7 +6,8 @@ mod common;
 use dracon_terminal_engine::utils::{
     delete_word_backwards, format_datetime_smart, format_permissions, format_size,
     get_file_category, get_open_with_suggestions, get_visual_width, guess_icon_mode,
-    is_binary_content, squarify, truncate_to_width, FileCategory, IconMode, SelectionState,
+    is_binary_content, move_recursive, squarify, truncate_to_width, FileCategory, IconMode,
+    SelectionState,
 };
 use ratatui::style::Color as RatatuiColor;
 use std::time::{Duration, SystemTime};
