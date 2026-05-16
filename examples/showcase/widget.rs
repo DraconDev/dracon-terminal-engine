09e546eb7780af8c0f6c2bf0e5200e9caf5b7b86
DraconDev <dracsharp@gmail.com>
2026-05-16 10:06:12 +0100

    fix(showcase): remove redundant status_message clear in widget render

--- examples/showcase/widget.rs
+++ examples/showcase/widget.rs
@@ -772,10 +772,6 @@ impl Widget for Showcase {
 
                 // Message text
                 draw_text(&mut plane, msg_x + 3, msg_y, msg, t.bg, t.warning, Styles::BOLD);
-            } else {
-                drop(msg);
-                drop(time);
-                self.status_message = None;
             }
         }
 
