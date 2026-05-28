1f44c9bbf2039840c77a47ba15cda37ac95ccbd5
DraconDev <dracsharp@gmail.com>
2026-05-28 10:30:45 +0100

    Modified examples/git_tui.rs
    1 file changed:
      Modified: examples/git_tui.rs

--- examples/git_tui.rs
+++ examples/git_tui.rs
@@ -1229,7 +1229,8 @@ fn main() -> std::io::Result<()> {
     let theme = Theme::from_env_or(Theme::nord());
     let git = GitTui::new(should_quit, theme.clone());
 
-    let mut app = App::new()?.title("Git TUI").fps(30).theme(theme);
+    let mut app = App::new()?.title("Git TUI").fps(30);
+    app.set_theme(theme);
     app.add_widget(Box::new(git), Rect::new(0, 0, w, h));
 
     app.on_tick(move |ctx, _| {
