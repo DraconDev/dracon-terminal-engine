88831c26b399bd045f144070d033c67935f206f9
DraconDev <dracsharp@gmail.com>
2026-05-31 16:27:24 +0100

    2 file(s) in src [src/input/mapping.rs, src/framework/widgets/mod.rs] DELTA:+0/-71

--- src/input/mapping.rs
+++ src/input/mapping.rs
@@ -7,28 +7,6 @@
 
 use crate::input::event::{Event, UiEvent};
 
-/// Converts a runtime event to a UI event (identity function).
-///
-/// This function is deprecated since event types are now unified.
-#[deprecated(
-    since = "19.3.0",
-    note = "Event types are now unified; this is an identity function"
-)]
-pub fn from_runtime_event(event: crate::input::event::Event) -> Event {
-    event
-}
-
-/// Converts a UI event to a runtime event (identity function).
-///
-/// This function is deprecated since event types are now unified.
-#[deprecated(
-    since = "19.3.0",
-    note = "Event types are now unified; this is an identity function"
-)]
-pub fn to_runtime_event(event: &Event) -> crate::input::event::Event {
-    event.clone()
-}
-
 /// Converts an event to a UI event if applicable.
 pub fn to_ui_event(event: &Event) -> Option<UiEvent> {
     event.to_ui_event()
@@ -37,40 +15,6 @@ pub fn to_ui_event(event: &Event) -> Option<UiEvent> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_from_runtime_event_identity() {
-        let event = Event::Key(crate::input::event::KeyEvent {
-            kind: crate::input::event::KeyEventKind::Press,
-            code: crate::input::event::KeyCode::Char('a'),
-            modifiers: crate::input::event::KeyModifiers::empty(),
-        });
-        #[allow(deprecated)]
-        let result = from_runtime_event(event.clone());
-        match result {
-            Event::Key(k) => {
-                assert!(matches!(k.code, crate::input::event::KeyCode::Char('a')));
-            }
-            _ => panic!("expected Key event"),
-        }
-    }
-
-    #[test]
-    fn test_to_runtime_event_identity() {
-        let event = Event::Key(crate::input::event::KeyEvent {
-            kind: crate::input::event::KeyEventKind::Press,
-            code: crate::input::event::KeyCode::Char('b'),
-            modifiers: crate::input::event::KeyModifiers::empty(),
-        });
-        #[allow(deprecated)]
-        let result = to_runtime_event(&event);
-        match result {
-            Event::Key(k) => {
-                assert!(matches!(k.code, crate::input::event::KeyCode::Char('b')));
-            }
-            _ => panic!("expected Key event"),
-        }
-    }
 
     #[test]
     fn test_to_ui_event_key() {
